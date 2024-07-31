6cbc25b5a1047bd3ec7889b636ff895d78654709
ObikeMartins <chiemezieobike@gmail.com>
2024-07-31 00:56:45 +0000

    commit abigail.tf

--- abigail.tf
+++ abigail.tf
@@ -10,7 +10,7 @@ provider "aws" {
 
 # Define an EC2 instance
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2 AMI
+  ami           = "ami-0c55b159cbfafe1f0" # ubuntu
   instance_type = "t2.micro"
 
   tags = {
